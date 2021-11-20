acd01b70aabf6f5eec9ecdf3a29b89ffce0d9af3
Italo Bejarano <italo.bejarano@diversolatam.com>
2021-11-20 18:01:21 -0500

    envs ambientes

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,12 +9,14 @@ import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 
+import { enviroments } from './enviroments';
+
 import { firstValueFrom } from 'rxjs';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env',
+      envFilePath: enviroments[process.env.NODE_ENV] || '.env',
       isGlobal: true,
     }),
     HttpModule,
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,15 +1,19 @@
 import { Injectable, Inject } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
   constructor(
     @Inject('API_KEY') private apiKey: string,
     @Inject('TASKS') private tasks: any[],
+    private configService: ConfigService,
   ) {}
   getHello(): object {
+    const apiKey = this.configService.get<string>('API_KEY');
+    const dbName = this.configService.get<string>('DATABASE_NAME');
     return {
-      message: `Hello world ${this.apiKey}`,
-      tasks: this.tasks,
+      apiKey: `${apiKey}`,
+      dbName: `${dbName}`,
     };
   }
 }
