e796754375397bc4221e5fc141961794ad2d00e3
Italo Bejarano <italo.bejarano@diversolatam.com>
2021-11-20 15:06:03 -0500

    modularizacion

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,35 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { BrandsController } from './brands/controllers/brands.controller';
-import { CategoriesController } from './categories/controllers/categories.controller';
-import { CustomersController } from './customers/controllers/customers.controller';
-import { ProductsController } from './products/controllers/products.controller';
-import { UsersController } from './users/controllers/users.controller';
-
-import { BrandsService } from './brands/services/brands.service';
-import { CategoriesService } from './categories/services/categories.service';
-import { CustomersService } from './customers/services/customers.service';
-import { ProductsService } from './products/services/products.service';
-import { UsersService } from './users/services/users.service';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [
-    AppController,
-    BrandsController,
-    CategoriesController,
-    CustomersController,
-    ProductsController,
-    UsersController,
-  ],
-  providers: [
-    AppService,
-    BrandsService,
-    CategoriesService,
-    CustomersService,
-    ProductsService,
-    UsersService,
-  ],
+  imports: [UsersModule, ProductsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
