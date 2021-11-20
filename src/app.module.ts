faf7fa44589e31e2d927ede11da39d85fd4bc13b
Italo Bejarano <italo.bejarano@diversolatam.com>
2021-11-20 18:20:05 -0500

    validacion env con Joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,28 @@
 import { Module } from '@nestjs/common';
 import { HttpModule, HttpService } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { firstValueFrom } from 'rxjs';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
-
 import { enviroments } from './enviroments';
-
-import { firstValueFrom } from 'rxjs';
+import config from './config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: enviroments[process.env.NODE_ENV] || '.env',
       isGlobal: true,
+      load: [config],
+      validationSchema: Joi.object({
+        API_KEY: Joi.number().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      }),
     }),
     HttpModule,
     UsersModule,
