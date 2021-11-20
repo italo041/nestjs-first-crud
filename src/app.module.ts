28834a7cd6e16ffef56887826c29b941c0f3913f
Italo Bejarano <italo.bejarano@diversolatam.com>
2021-11-20 14:49:01 -0500

    update name controller to controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { BrandsController } from './brands/controller/brands.controller';
-import { CategoriesController } from './categories/controller/categories.controller';
-import { CustomersController } from './customers/controller/customers.controller';
-import { ProductsController } from './products/controller/products.controller';
-import { UsersController } from './users/controller/users.controller';
+import { BrandsController } from './brands/controllers/brands.controller';
+import { CategoriesController } from './categories/controllers/categories.controller';
+import { CustomersController } from './customers/controllers/customers.controller';
+import { ProductsController } from './products/controllers/products.controller';
+import { UsersController } from './users/controllers/users.controller';
 
 import { BrandsService } from './brands/services/brands.service';
 import { CategoriesService } from './categories/services/categories.service';
--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -9,9 +9,9 @@ import {
   // ParseIntPipe,
 } from '@nestjs/common';
 
-import { ParseIntPipe } from './../../common/parse-int.pipe';
-import { ProductsService } from './../services/products.service';
-import { CreateProductDto, UpdateProductDto } from './../dtos/products.dtos';
+import { ParseIntPipe } from '../../common/parse-int.pipe';
+import { ProductsService } from '../services/products.service';
+import { CreateProductDto, UpdateProductDto } from '../dtos/products.dtos';
 
 @Controller('products')
 export class ProductsController {
